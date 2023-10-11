add53d3a92c30a848d0e4abf5f2c80d3d14e8660
Vladislav Nikolaev <NikolaevVP@tatneft.tatar>
2023-10-11 09:11:54 +0300

    Remove dumb project sources

--- src/components/@uikit/AlertButton/AlertButton.ts
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-	document.querySelectorAll(".alert-button").forEach((element) => {
-		element.addEventListener("click", (e) => {
-			// @ts-ignore
-			alert(e.currentTarget.textContent);
-		});
-	});
-});
--- src/components/@uikit/Button/Button.ts
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", () => {
-	document.querySelectorAll(".button").forEach((element) => {
-		element.addEventListener("click", () => console.log("click"));
-	});
-});
