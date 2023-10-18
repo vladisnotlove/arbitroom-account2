fd970cca23da569d1ad562f264dfd5759782808d
Vladislav Nikolaev <vladisnotlove@gmail.com>
2023-10-18 21:19:06 +0300

    Add Operations

---
+++ src/components/Operations/data.ts
@@ -0,0 +1,52 @@
+export const columns = ["Операции", "Статус", "Сумма", "Дата", "Комментарий"];
+
+export const data: Array<{
+	operation: string;
+	status: "inProgress" | "success" | "error";
+	sum: string;
+	date: string;
+	comment: string;
+}> = [
+	{
+		operation: "Вывод личных средств",
+		status: "inProgress",
+		sum: "14 244 USDT",
+		date: "10.06.2021",
+		comment: "Комментарий название",
+	},
+	{
+		operation: "Вывод партнерских средств",
+		status: "error",
+		sum: "14 244 USDT",
+		date: "10.06.2021",
+		comment: "Комментарий название",
+	},
+	{
+		operation: "Перевод",
+		status: "success",
+		sum: "14 244 USDT",
+		date: "10.06.2021",
+		comment: "Комментарий название",
+	},
+	{
+		operation: "Вывод личных средств",
+		status: "success",
+		sum: "14 244 USDT",
+		date: "10.06.2021",
+		comment: "Комментарий название",
+	},
+	{
+		operation: "Вывод личных средств",
+		status: "success",
+		sum: "14 244 USDT",
+		date: "10.06.2021",
+		comment: "Комментарий название",
+	},
+	{
+		operation: "Вывод личных средств",
+		status: "success",
+		sum: "14 244 USDT",
+		date: "10.06.2021",
+		comment: "Комментарий название",
+	},
+];
