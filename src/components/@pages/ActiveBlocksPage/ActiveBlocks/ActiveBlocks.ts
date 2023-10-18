de3f4fa760026a3b68ddca2ecd1c73150cfe385e
Vladislav Nikolaev <vladisnotlove@gmail.com>
2023-10-18 21:47:53 +0300

    Make ActiveBlocksPage adaptive + small fixes

--- src/components/@pages/ActiveBlocksPage/ActiveBlocks/ActiveBlocks.ts
+++ src/components/@pages/ActiveBlocksPage/ActiveBlocks/ActiveBlocks.ts
@@ -21,18 +21,38 @@ window.addEventListener("load", () => {
 
 		if (blocks && input && activeBtn) {
 			blocks.forEach((block) => {
-				block.addEventListener("focus", () => {
+				const selectBlock = () => {
+					// set selected
+					blocks.forEach((block) => block.classList.remove("selected"));
+					block.classList.add("selected");
+
+					// set value in input
 					const value = block.getAttribute("data-value");
 					if (value) {
 						input.value = value;
 						input.dispatchEvent(new Event("change")); // trigger change handlers
-						activeBtn.classList.remove("disabled");
 					}
-				});
-				block.addEventListener("blur", () => {
+
+					// enable button
+					activeBtn.classList.remove("disabled");
+				};
+
+				const unselectBlock = () => {
+					block.classList.remove("selected");
 					input.value = "";
 					input.dispatchEvent(new Event("change")); // trigger change handlers
 					activeBtn.classList.add("disabled");
+				};
+
+				block.addEventListener("click", (e) => {
+					e.stopPropagation();
+					selectBlock();
+				});
+
+				document.addEventListener("click", (e) => {
+					if (e.target instanceof HTMLElement && !e.target.closest(".active-blocks__active-btn")) {
+						unselectBlock();
+					}
 				});
 			});
 		}
