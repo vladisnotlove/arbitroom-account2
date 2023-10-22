b96233e9f5a777e508b54a19aaf735589485aea2
Vladislav Nikolaev <vladisnotlove@gmail.com>
2023-10-22 17:06:08 +0300

    Complete outcome page

--- src/components/@uikit/ToggleButtonGroup/ToggleButtonGroup.ts
+++ src/components/@uikit/ToggleButtonGroup/ToggleButtonGroup.ts
@@ -31,58 +31,60 @@ const updateActive = (elements: { toggleButtonGroup: HTMLElement; target?: Eleme
 // MAIN
 
 window.addEventListener("load", () => {
-	document.querySelectorAll(".toggle-button-group:not([data-disable-js])").forEach((toggleButtonGroup) => {
-		if (!(toggleButtonGroup instanceof HTMLElement)) {
-			console.error(".toggleButtonGroup is not a HTMLElement", toggleButtonGroup);
-			return;
-		}
-
-		const input = toggleButtonGroup.querySelector("input");
-		const buttons = Array.from(toggleButtonGroup.querySelectorAll(".toggle-button"));
-
-		const str = toggleButtonGroup.getAttribute("data-toggle-button-group-target");
-		const target = str ? Array.from(document.querySelectorAll(str)) : undefined;
-
-		toggleButtonGroup.addEventListener("click", (e) => {
-			if (!(e.target instanceof HTMLElement) || !e.target.closest(".toggle-button")) return;
-
-			const activeButton = e.target;
-			const value = activeButton.getAttribute("data-value");
-
-			// set value in input
-			if (input && value !== null) {
-				setValue({ input }, value);
+	document
+		.querySelectorAll(".toggle-button-group:not([data-disable-toggle-button-group-js])")
+		.forEach((toggleButtonGroup) => {
+			if (!(toggleButtonGroup instanceof HTMLElement)) {
+				console.error(".toggleButtonGroup is not a HTMLElement", toggleButtonGroup);
+				return;
 			}
 
-			// change active button
-			buttons.forEach((button) => {
-				if (button === activeButton) {
-					button.classList.add("active");
-				} else {
-					button.classList.remove("active");
+			const input = toggleButtonGroup.querySelector("input");
+			const buttons = Array.from(toggleButtonGroup.querySelectorAll(".toggle-button"));
+
+			const str = toggleButtonGroup.getAttribute("data-toggle-button-group-target");
+			const target = str ? Array.from(document.querySelectorAll(str)) : undefined;
+
+			toggleButtonGroup.addEventListener("click", (e) => {
+				if (!(e.target instanceof HTMLElement) || !e.target.closest(".toggle-button")) return;
+
+				const activeButton = e.target;
+				const value = activeButton.getAttribute("data-value");
+
+				// set value in input
+				if (input && value !== null) {
+					setValue({ input }, value);
+				}
+
+				// change active button
+				buttons.forEach((button) => {
+					if (button === activeButton) {
+						button.classList.add("active");
+					} else {
+						button.classList.remove("active");
+					}
+				});
+
+				// change active target
+				if (target) {
+					const activeIndex = buttons.findIndex((button) => button === activeButton);
+					target.forEach((elem) => {
+						Array.from(elem.children).forEach((child, index) => {
+							if (index === activeIndex) {
+								child.classList.add("active");
+							} else {
+								child.classList.remove("active");
+							}
+						});
+					});
 				}
 			});
 
-			// change active target
-			if (target) {
-				const activeIndex = buttons.findIndex((button) => button === activeButton);
-				target.forEach((elem) => {
-					Array.from(elem.children).forEach((child, index) => {
-						if (index === activeIndex) {
-							child.classList.add("active");
-						} else {
-							child.classList.remove("active");
-						}
-					});
+			if (input) {
+				updateActive({ input, target, toggleButtonGroup });
+				input.addEventListener("change", () => {
+					updateActive({ input, target, toggleButtonGroup });
 				});
 			}
 		});
-
-		if (input) {
-			updateActive({ input, target, toggleButtonGroup });
-			input.addEventListener("change", () => {
-				updateActive({ input, target, toggleButtonGroup });
-			});
-		}
-	});
 });
