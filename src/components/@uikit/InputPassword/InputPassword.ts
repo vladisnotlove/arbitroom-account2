8048509070272be2b8a53d31bf83b299c4722975
Vladislav Nikolaev <vladisnotlove@gmail.com>
2023-10-30 01:37:34 +0300

    Fix InputPassword

--- src/components/@uikit/InputPassword/InputPassword.ts
+++ src/components/@uikit/InputPassword/InputPassword.ts
@@ -1,25 +1,25 @@
 window.addEventListener("load", () => {
-	document.querySelectorAll("input-password").forEach((root) => {
+	document.querySelectorAll(".input-password").forEach((root) => {
 		const input = root.querySelector("input");
 		const eyeBtn = root.querySelector(".input-password__eye-btn");
 
 		if (!input || !eyeBtn) return;
 
 		const turnEyeOn = () => {
-			input.classList.add("eye-on");
-			input.classList.remove("eye-off");
+			root.classList.add("eye-on");
+			root.classList.remove("eye-off");
 			input.setAttribute("type", "text");
 		};
 
 		const turnEyeOff = () => {
-			input.classList.remove("eye-on");
-			input.classList.add("eye-off");
+			root.classList.remove("eye-on");
+			root.classList.add("eye-off");
 			input.setAttribute("type", "password");
 		};
 
 		eyeBtn.addEventListener("click", (e) => {
 			e.stopPropagation();
-			if (input.classList.contains("eye-on")) {
+			if (root.classList.contains("eye-on")) {
 				turnEyeOff();
 			} else {
 				turnEyeOn();
